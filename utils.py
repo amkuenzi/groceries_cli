30b5edea92f432eb7c48f2045206a35d6b909ee9
Arthur Kuenzi <arthurkuenzi@hotmail.co.uk>
2026-01-18 18:00:47 +0000

    added utils: select_data_source wrapper allows funcitons to take storage interface (with context manager) or raw data as input

---
+++ utils.py
@@ -0,0 +1,23 @@
+"""General utilities"""
+
+from typing import List, Dict, Union
+from functools import wraps
+
+from storage import StorageInterface, StorageData
+
+
+def select_data_source(func):
+    @wraps(func)
+    def wrapper(data_source: Union[StorageInterface, StorageData], *args, **kwargs):
+
+        if isinstance(data_source, StorageInterface):
+            with data_source.storage_data() as data:
+                return func(data, *args, *kwargs)
+            
+        elif isinstance(data_source, StorageData):
+            return func(data, *args, **kwargs)
+        
+        else:
+            raise ValueError(f"{repr(func)} data source must be of type: {repr(StorageInterface)} or {repr(StorageData)}")
+    
+    return wrapper
