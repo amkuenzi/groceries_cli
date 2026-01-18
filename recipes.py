4deaa529d24be7ad9f87e1fdcb35482b6bb17c82
Arthur Kuenzi <arthurkuenzi@hotmail.co.uk>
2026-01-18 18:16:05 +0000

    added data source selection to recipe functions

--- recipes.py
+++ recipes.py
@@ -19,60 +19,54 @@ TODO: refactor into service classes instead of functions
     TODO: add prompt to generate new recipe item
 TODO: Add transaction-style batching, also to pantry, planner
 """
-from typing import List, Union
+from typing import List
 
-from storage import Storage, StorageData     # Storage class prototype
+from storage import StorageData     # Storage class prototype
 from models import Recipe
-
+from utils import select_data_source
 
 class RecipeExistsError(Exception):
     pass
 
-
 class RecipeNotFoundError(Exception):
     pass
 
+@select_data_source
+def add_recipe(data: StorageData, new_recipe: Recipe) -> tuple[StorageData, str]:
+    new_recipe.name = new_recipe.name.lower()
+    if new_recipe.name in data.recipes:
+        raise RecipeExistsError(f"recipe: '{new_recipe.name}' already exists")
+    
+    data.recipes[new_recipe.name] = new_recipe
+    return data, f'RecipeLib: added {new_recipe.name.title()}'
 
-def add_recipe(storage: Storage, new_recipe: Recipe) -> str:
-    with storage.storage_data() as data:
-        # Prevent duplicate names
-        if new_recipe.name.lower() in data.recipes:
-            raise RecipeExistsError(f"recipe: '{new_recipe.name}' already exists")
-        
-        # add recipe
-        data.recipes[new_recipe.name.lower()] = new_recipe
-    return f'Added {new_recipe.name.lower()}'
+@select_data_source
+def remove_recipe(data: StorageData, name: str) -> tuple[StorageData, str]:
+    if name.lower() not in data.recipes:
+        raise RecipeNotFoundError(name.lower)
 
+    del data.recipes[name.lower()]
+    return data, f"RecipeLib: deleted {name.lower()}"
 
-def get_recipe(storage: Storage, name: str) -> Recipe:
-    with storage.storage_data() as data:
-        try:
-            retrieved =  data.recipes[name.lower()]
-        except KeyError:
-            return RecipeNotFoundError(name)
+@select_data_source
+def update_recipe(data: StorageData, new_recipe: Recipe) -> tuple[StorageData, str]:
+    new_recipe.name.lower()
+    data.recipes[new_recipe.name] = new_recipe
+    if new_recipe.name.lower() in data.recipes:
+        message = f'Recipe Lib: updated {new_recipe.name.title()}'
+    else:
+        message = f'Recipe Lib: added {new_recipe.name.title()}'
+    return data, message
+
+@select_data_source
+def get_recipe(data: StorageData, name: str) -> Recipe:
+    try:
+        retrieved = data.recipes[name.lower()]
+    except KeyError:
+        return RecipeNotFoundError(name)
     return retrieved
 
-
-def list_recipes(storage: Storage) -> List[Recipe]:
-    with storage.storage_data() as data:
-        recipe_list = list(data.recipes.values())
+@select_data_source
+def list_recipes(data: StorageData) -> List[Recipe]:
+    recipe_list = list(data.recipes.values())
     return recipe_list
-
-
-def update_recipe(storage: Storage, new_recipe: Recipe) -> str:
-    with storage.storage_data() as data:
-        data.recipes[new_recipe.name.lower()] = new_recipe
-        if new_recipe.name.lower() in data.recipes:
-            message = f'Updated: {new_recipe.name.lower()}'
-        else:
-            message = f'Added: {new_recipe.name.lower()}'
-    return message
-
-
-def delete_recipe(storage: Storage, name: str) -> str:
-    with storage.storage_data() as data:
-        if name.lower() not in data.recipes:
-            raise RecipeNotFoundError(name.lower)
-
-        del data.recipes[name.lower()]
-    return f"Deleted: {name.lower()}"
